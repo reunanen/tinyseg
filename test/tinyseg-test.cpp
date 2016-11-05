408c6e4f09337cefa936d6c4eb531c7c4d46575e
Juha Reunanen <juha.reunanen@tomaattinen.com>
2016-11-05 22:42:59 +0200

    Under construction... doesn't even compile right now.

--- test/tinyseg-test.cpp
+++ test/tinyseg-test.cpp
@@ -2,6 +2,15 @@
 
 int main(int argc, char* argv[])
 {
+    const std::vector<cv::Scalar> mask_colors = {
+        cv::Scalar(127, 127, 127),
+        cv::Scalar(255, 0, 0),
+        cv::Scalar(0, 255, 0),
+        cv::Scalar(0, 0, 255),
+    };
+
+    const auto& dataset = tinyseg::load_image("../test-images/1.jpg", "../test-images/1.png", mask_colors);
+
     return 0;
 }
 
--- tinyseg.hpp
+++ tinyseg.hpp
@@ -7,21 +7,31 @@
 namespace tinyseg {
 
 struct dataset {
-    std::vector<tiny_dnn::label_t> images;
-    std::vector<tiny_dnn::vec_t> labels;
+    std::vector<tiny_dnn::vec_t> inputs;
+    std::vector<tiny_dnn::label_t> labels;
+    std::vector<tiny_dnn::vec_t> weights;
 };
 
-dataset load_image(const std::string& input_file_name, const std::string& mask_file_name, std::vector<cv::Scalar> mask_colors) {
-    const cv::Mat input = cv::imread(input_file_name);
+dataset load_image(const std::string& input_file_name, const std::string& mask_file_name, const std::vector<cv::Scalar>& mask_colors) {
+    const cv::Mat_<uint8_t> input = cv::imread(input_file_name, cv::IMREAD_GRAYSCALE);
     const cv::Mat mask = cv::imread(mask_file_name);
+    cv::Mat_<uint16_t> labels(input.size(), 0);
+    cv::Mat_<uint8_t> weights(input.size(), 0);
 
     cv::Mat label_mask;
     for (tiny_dnn::label_t label = 0, label_count = mask_colors.size(); label < label_count; ++label) {
         const cv::Scalar& mask_color = mask_colors[label];
         cv::inRange(mask, mask_color, mask_color, label_mask);
+        labels.setTo(label, label_mask);
+        weights.setTo(1, label_mask);
     }
 
     dataset dataset;
+
+    std::transform(input.begin(), input.end(), std::back_inserter(dataset.inputs), [](uint8_t input) { return static_cast<tiny_dnn::float_t>(input); });
+    std::transform(labels.begin(), labels.end(), std::back_inserter(dataset.labels), [](uint16_t label) { return static_cast<tiny_dnn::float_t>(label); });
+    std::transform(weights.begin(), weights.end(), std::back_inserter(dataset.weights), [](uint8_t weight) { return static_cast<tiny_dnn::float_t>(weight); });
+
     return dataset;
 }
 
